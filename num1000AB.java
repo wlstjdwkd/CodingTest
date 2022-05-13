c677e11251b3a991b211b695419993ccb6589e0f
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2022-05-13 22:48:41 +0900

    1000번 A+B

---
+++ num1000AB.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class num1000AB {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = 0;
+        int b = 0;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
