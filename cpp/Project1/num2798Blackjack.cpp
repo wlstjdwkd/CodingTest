3ec7ac10067202e1542204fa8de279c3e7af4319
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2022-05-24 00:04:58 +0900

    fa

--- cpp/Project1/num2798Blackjack.cpp
+++ cpp/Project1/num2798Blackjack.cpp
@@ -6,22 +6,23 @@ int main() {
 	int a[100];
 	int sum = 0;
 	int max = 0;
-	/*for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++) {
 		cin >> a[i];
-	}*/
+	}
 	for (int i = 0; i < N; i++) {
 		sum = 0;
-		cin >> a[i];
+		//cin >> a[i];
+		sum += a[i];
 		for (int j = i + 1; j < N; j++) {
+			sum = a[i];
+			sum += a[j];
 			for (int k = j + 1; k < N; k++) {
-				if (sum + a[i] <= M) {
-					sum += a[i];
+				sum = a[i] + a[j];
+				if (sum + a[k] <= M) {
+					sum += a[k];
 					if (max <= sum) {
 						max = sum;
 					}
-				}
-				else {
-					continue;
 				}
 			}
 		}
