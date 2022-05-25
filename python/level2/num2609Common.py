e6deefd71c182c00e32012dbd4bf16d5368cf0be
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2022-05-26 00:04:25 +0900

    220526

--- python/level2/num2609Common.py
+++ python/level2/num2609Common.py
@@ -1,5 +1,18 @@
-a, b = map(int, input().split())
-max=0
-min=0
-for i in a:
-    if i/a==0 and i/b==0:
+# a, b = map(int, input().split())
+# max=0
+# min=0
+# c=0
+# d=0
+# for i in range(a):
+#     if a%i==0 and b%i==0:
+#         min=i
+#         c=i/a
+#         d=i/b
+#
+# max=c*d*min
+# print(min)
+# print(max)
+import math
+a,b = map(int, input().split())
+print(math.gcd(a,b))
+print(math.lcm(a,b))
