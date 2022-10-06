d7e9ac46a980d634b6a314525b3f7abf333d93f6
xkakrlfh@naver.com <xkakrlfh@naver.com>
2022-10-07 00:26:58 +0900

    221007

--- python/프로그래머스/12978Delivery.py
+++ python/프로그래머스/12978Delivery.py
@@ -9,15 +9,20 @@ def dijkstra(dist, adj):
     while q:
         cost, node = heapq.heappop(q)
         for c, n in adj[node]:
+            #현재 노드를 거쳐서 다른노드를 가는게 더빠른 경우
             if cost+c < dist[n]:
                 dist[n] = cost+c
                 heapq.heappush(q, (cost+c, n))
 
 
 def solution(N, road, K):
+    #1에서의 거리
     dist = [float('inf')] * (N+1)
     dist[1] = 0
+
+    #인접노드랑 인접 거리
     adj = [[] for i in range(N+1)]
+
     for r in road:
         adj[r[0]].append((r[2], r[1]))
         adj[r[1]].append((r[2], r[0]))
