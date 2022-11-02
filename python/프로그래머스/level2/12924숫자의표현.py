63a5d1eb60575d674925e4181592056a0d88aac8
xkakrlfh@naver.com <xkakrlfh@naver.com>
2022-11-02 22:05:24 +0900

    221102

---
+++ python/프로그래머스/level2/12924숫자의표현.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1,n//2+1):
+        total=0
+        for j in range(i,n+1):
+            total+=j
+            if total==n:
+                answer+=1
+                break
+            elif total>n:
+                break
+    return answer+1
+
+print(solution(15))
