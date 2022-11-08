85614fd1989716f1bca6b4937ae2828b409b80c2
xkakrlfh@naver.com <xkakrlfh@naver.com>
2022-11-08 20:42:58 +0900

    221108

--- python/프로그래머스/level2/42883큰수만들기.py
+++ python/프로그래머스/level2/42883큰수만들기.py
@@ -1,14 +1,39 @@
 def solution(number, k):
-    stack= [number[0]]
-    for n in number[1:]:
-        while stack and int(n)>int(stack[-1]) and k>0:
-            stack.pop()
+
+    # answer = ""
+    # q = list(number)
+    # i = 0
+    # while True:
+    #     if q[i] == '9':
+    #         i += 1
+    #         continue
+        
+    #     if q[i+1] > q[i]:
+    #         del q[i]
+    #         k -= 1
+    #         if k ==0:
+    #             break
+    #         i = 0
+    #     else:
+    #         i += 1
+    #         if i == len(q)-1:
+    #             for j in range(k):
+    #                 del q[-1]
+    #             k = 0
+    #             break
+
+    # answer += "".join(q)
+
+    # return answer
+    stack=[number[0]]
+    for num in number[1:]:
+        while len(stack) >0 and k>0 and stack[-1]<num:
             k-=1
-        stack.append(n)
+            stack.pop()
+        stack.append(num)
+    if k>0:
+        stack = stack[:-k]
+    return "".join(stack)
 
-        if len(stack) == len(number)-k:
-            break
-    
-    return ''.join(stack)
 
-print(solution("1924",2))
+print(solution("4177252841", 4))
