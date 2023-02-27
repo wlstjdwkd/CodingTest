8292a2249e0ccdfb1d31f8eaac8bdb92614f8268
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2023-02-27 16:57:33 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 80 ms, Memory: 11712 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,59 +1,43 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int N;
-    static int[][] arr;
-    static int result;
-    public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
 
-        for(int i=0; i<N; i++){
+    private static int N, map[][], dp[][][];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        dp = new int[N][N][3];
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                arr[i][j]= Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++)
+                map[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0][1][0] = 1;
+        System.out.println(dp());
+    }
+
+    private static int dp() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 2; j < N; j++) {
+
+                if (map[i][j] == 1) continue;
+                dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][1];
+
+                if (i == 0) continue;;
+                dp[i][j][2] = dp[i - 1][j][1] + dp[i - 1][j][2];
+
+                if (map[i - 1][j] == 1 || map[i][j - 1] == 1) continue;
+                dp[i][j][1] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
             }
         }
-
-        result=0;
-        dfs(0,1,0);
-
-        System.out.println(result);
-
+        return dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2];
     }
-    private static void dfs(int x, int y, int direction){
-        if(x==N-1 && y==N-1){
-            result++;
-            return;
-        }
-        switch(direction){
-            case 0://가로
-                if(y+1<N && arr[x][y+1]==0){
-                    dfs(x,y+1,0);
-                }
-                break;
-            case 1: //세로
-                if(x+1<N && arr[x+1][y]==0){
-                    dfs(x+1,y,1);
-                }
-                break;
-            case 2: //대각선
-                if(y+1<N && arr[x][y+1]==0){
-                    dfs(x,y+1,0);
-                }
-                if(x+1<N && arr[x+1][y]==0){
-                    dfs(x+1,y,1);
-                }
-                break;
-        }
-        if(y+1<N && x+1<N && arr[x+1][y+1]==0 && arr[x][y+1]==0 && arr[x+1][y]==0){
-            dfs(x+1,y+1,2);
-        }
-    }
-
 }
