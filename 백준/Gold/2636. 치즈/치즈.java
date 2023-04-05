28bc061356889684a6f4b404f419994854409d76
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2023-04-05 13:32:04 +0900

    [Gold IV] Title: 치즈, Time: 156 ms, Memory: 15224 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,69 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int N,M, arr[][];
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static boolean[][] visited;
-    static int cheese;
+	static int n, m;
+	static int[][] cheese;
+	static int initialCheeseCount = 0, cheeseCount = 0;
+	static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
+	static boolean[][] visited;
+	
+	public static void main(String[] args) throws IOException {
+		
+		/* 입력 및 변수 초기화 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		cheese = new int[n][m];
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {		
+				cheese[i][j] = Integer.parseInt(st.nextToken());
+				if(cheese[i][j] == 1) {
+					initialCheeseCount++;
+				}
+			}
+		}
+		
+		int time = 0, lastCheeseCount = 0;
+		
+		while(cheeseCount < initialCheeseCount) {
+			time++;
+			lastCheeseCount = initialCheeseCount - cheeseCount;
+			meltCheese();
+		}
+		
+		/* 출력 */
+		System.out.println(time);
+		System.out.println(lastCheeseCount);
+		
+	}
+	
+	/* BFS : 공기와 맞닿은 치즈 조각 녹이기 */
+	private static void meltCheese() {
+		visited = new boolean[n][m];
+		Deque<int[]> q = new ArrayDeque<>();
+		q.add(new int[]{0, 0});
+		
+		while(!q.isEmpty()) {
+			int[] point = q.poll();
 
-    public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr= new int[N][M];
-        visited = new boolean[N][M];
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<M; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                if(arr[i][j]==1){
-                    cheese++;
-                }
-            }
-        }
-        int cheeseCount=0;
-        int time=0;
-        while(cheese!=0){
-            cheeseCount=cheese;
-            time++;
-            visited=new boolean[N][M];
-            bfs();
-        }
-        System.out.println(time);
-        System.out.println(cheeseCount);
+			for(int i=0; i<4; i++) {
+				int nx = dx[i] + point[0];
+				int ny = dy[i] + point[1];
+				
+				if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+				if(visited[nx][ny]) continue;
+				
+				visited[nx][ny] = true;
+				
+				// 치즈 조각인 경우
+				if(cheese[nx][ny] == 1) {
+					cheese[nx][ny] = 0;
+					cheeseCount++;
+					continue;
+				}
+				
+				// 치즈가 아닌 경우
+				q.add(new int[] {nx, ny});
+			}
+		}
+	}
 
-    }
-    private static void bfs(){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0, 0});
-        visited[0][0] = true;
-
-        while(!q.isEmpty()) {
-            int[] current = q.poll();
-
-            for(int i = 0; i < 4; i++) {
-                int nx = current[0] + dx[i];
-                int ny = current[1] + dy[i];
-
-                if(nx >= 0 && ny >= 0 && nx < N && ny < M && visited[nx][ny] == false) {
-                    visited[nx][ny] = true;
-                    
-                    if(arr[nx][ny] == 0) {
-                        q.offer(new int[] {nx, ny});
-                    } else {
-                        cheese--;
-                        arr[nx][ny] = 0;
-                    }
-                }
-            }
-        }
-    }
 }
