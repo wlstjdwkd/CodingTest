0892ea927c474618d1963635262b7d90bc23eda1
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2023-04-10 14:26:52 +0900

    [level 3] Title: 숫자 게임, Time: 63.65 ms, Memory: 64.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+ 
+        int aPoint = 0;
+        int bPoint = 0;
+        int result = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[aPoint] >= B[bPoint]) {
+                bPoint++;
+            } else {
+                aPoint++;
+                bPoint++;
+                result++;
+            }
+        }
+        return result;
+    }
+}
