a40277db3bea89a2ce46e1fb54af7ff3ae3fdd97
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2023-04-12 16:44:41 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 432 ms, Memory: 75992 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,156 +1,133 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int R,C,T;
-    static int[][] map;
-    static int cleaner = -1;
-    static int[] dx = {0, -1, 0, 1}, dy = {1, 0, -1, 0};
-    static Queue<Dust> dusts;
 
-    static class Dust{
-        int x,y,w;
-
-        public Dust(int x, int y, int w) {
-            this.x = x;
-            this.y = y;
-            this.w = w;
-        }
-    }
+    static int r, c;
+    static int[][] dust;
+    static int airCleaner1, airCleaner2; // 공기청정기 위치
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        T = Integer.parseInt(st.nextToken());
-        map = new int[R][C];
-        for(int i=0; i<R; i++){
+
+        /* 입력 및 변수 초기화 */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+        dust = new int[r][c];
+        for(int i=0; i<r; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<C; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-
-                if(cleaner==-1 && map[i][j]==-1){
-                    cleaner =i;
+            for(int j=0; j<c; j++) {
+                dust[i][j] = Integer.parseInt(st.nextToken());
+                if(dust[i][j] == -1) {
+                    airCleaner2 = i;
                 }
             }
         }
+        airCleaner1 = airCleaner2 - 1;
 
-        //시간동안 확산
-        for(int i=0; i<T; i++){
-            checkDust();
-
-            spread();
-
-            move();
+        /* t초 동안 실행 */
+        while(t-- > 0) {
+            spreadDust();
+            purifyAir();
         }
 
-        int res=0;
-        for(int i=0; i<R; i++){
-            for(int j=0; j<C; j++){
-                if(map[i][j]!=-1){
-                    res+= map[i][j];
+        /* 출력 */
+        System.out.println(getDustAmout());
+    }
+
+    /* 미세먼지 확산 */
+    private static void spreadDust() {
+        int[][] newDust = new int[r][c];
+        // 공기청정기 복사
+        newDust[airCleaner1][0] = -1;
+        newDust[airCleaner2][0] = -1;
+
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                // 미세먼지가 있는 경우
+                if(dust[i][j] > 0) {
+                    newDust[i][j] += dust[i][j];
+                    for(int k=0; k<4; k++) {
+                        int x = dx[k] + i;
+                        int y = dy[k] + j;
+
+                        if(x<0 || x>=r || y<0 || y>=c) continue; // 범위 벗어날 경우
+                        if(dust[x][y] == -1) continue; // 공기 청정기일 경우
+
+                        newDust[x][y] += dust[i][j]/5;
+                        newDust[i][j] -= dust[i][j]/5;
+                    }
                 }
             }
         }
-        System.out.println(res);
+        dust = cloneDust(newDust);
     }
 
-    private static void checkDust(){
-        dusts = new ArrayDeque<>();
-        for(int i=0; i<R; i++){
-            for(int j=0; j<C; j++){
-                if(map[i][j] ==0|| map[i][j]==-1){
-                    continue;
-                }
-                dusts.add(new Dust(i,j,map[i][j]));
-            }
+    /* dust 배열 복사 */
+    private static int[][] cloneDust(int[][] cpDust) {
+        int[][] tmpDust = new int[r][c];
+        for(int i=0; i<r; i++) {
+            tmpDust[i] = cpDust[i].clone();
+        }
+        return tmpDust;
+    }
+
+    /* 공기 청정기 작동 */
+    private static void purifyAir() {
+        int[][] newDust = cloneDust(dust);
+
+        /* 시계방향으로 작동 */
+        dust[airCleaner2][1] = 0;
+        // 좌 -> 우
+        for(int j=2; j<c; j++) {
+            dust[airCleaner2][j] = newDust[airCleaner2][j-1];
+        }
+        // 상 -> 하
+        for(int i=airCleaner2+1; i<r; i++) {
+            dust[i][c-1] = newDust[i-1][c-1];
+        }
+        // 우 -> 좌
+        for(int j=c-2; j>=0; j--) {
+            dust[r-1][j] = newDust[r-1][j+1];
+        }
+        // 하 -> 상
+        for(int i=r-2; i>airCleaner2; i--) {
+            dust[i][0] = newDust[i+1][0];
+        }
+
+        /* 반시계방향으로 작동 */
+        dust[airCleaner1][1] = 0;
+        // 좌 -> 우
+        for(int j=2; j<c; j++) {
+            dust[airCleaner1][j] = newDust[airCleaner1][j-1];
+        }
+        // 하 -> 상
+        for(int i=airCleaner1-1; i>=0; i--) {
+            dust[i][c-1] = newDust[i+1][c-1];
+        }
+        // 우 -> 좌
+        for(int j=c-2; j>=0; j--) {
+            dust[0][j] = newDust[0][j+1];
+        }
+        // 상 -> 하
+        for(int i=1; i<airCleaner1; i++) {
+            dust[i][0] = newDust[i-1][0];
         }
     }
 
-    private static void spread(){
-        while(!dusts.isEmpty()){
-            Dust now = dusts.poll();
-
-            if(now.w <5){
-                continue;
-            }
-            int amountOfSpread = now.w/5;
-            int cnt=0;
-            for(int d=0; d<4; d++) {
-                int xx = now.x + dx[d];
-                int yy = now.y + dy[d];
-                if (xx < 0 || xx >= R || yy < 0 || yy >= C) continue;
-                // 공기청정기가 있으면
-                if (map[xx][yy] == -1) continue;
-
-                if(isInArea(xx,yy)&&!isCleaner(xx,yy)){
-                map[xx][yy] += amountOfSpread;
-                ++cnt;
-            }
-            }
-                map[now.x][now.y] -= amountOfSpread * cnt;
+    /* 미세먼지의 양 출력*/
+    private static int getDustAmout() {
+        int amount = 0;
+        for(int[] dst : dust) {
+            for(int d : dst) {
+                if(d == -1) continue;
+                amount += d;
             }
         }
-
-
-    private static void move(){
-        int top = cleaner;
-        int down = cleaner+1;
-
-        //윗부분
-        //아래로 내리기
-        for(int i= top-1; i>0; i--){
-            map[i][0] = map[i-1][0];
-        }
-        //왼쪽으로
-        for(int i=0; i<C-1; i++){
-            map[0][i] = map[0][i+1];
-        }
-        // 위로 당기기
-        for (int i = 0; i < top; i++){
-            map[i][C - 1] = map[i + 1][C - 1];
-        }
-        //오른쪽으로
-        for(int i=C-1; i>1; i--){
-            map[top][i] = map[top][i-1];
-        }
-        map[top][1]=0;
-
-        // 아래쪽
-        // 위로 당기기
-        for (int i = down + 1; i < R - 1; i++){
-            map[i][0] = map[i + 1][0];
-        }
-        // 왼쪽으로 당기기
-        for (int i = 0; i < C - 1; i++){
-            map[R - 1][i] = map[R - 1][i + 1];
-        }
-        // 아래로 당기기
-        for (int i = R - 1; i > down; i--){
-            map[i][C - 1] = map[i - 1][C - 1];
-
-        }
-        // 오른쪽으로 당기기
-        for (int i = C - 1; i > 1; i--){
-            map[down][i] = map[down][i - 1];
-        }
-        // 공기청정기에서 부는 바람은 미세먼지가 없는 바람
-        map[down][1] = 0;
+        return amount;
     }
-
-    private static boolean isInArea(int x, int y){
-        return 0<=x && x<R && 0<=y && y<C;
-    }
-
-    private static boolean isCleaner(int x, int y){
-        return map[x][y]==-1;
-    }
-
 }
