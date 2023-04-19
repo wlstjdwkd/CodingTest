9d764857ab0a5d72e2b8061024548fcc39b588c2
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2023-04-19 17:39:13 +0900

    [Gold II] Title: 트리의 순회, Time: 1348 ms, Memory: 78252 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -1,35 +1,39 @@
-import java.util.*;    
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-    }
-}
-class Solution{
-    int n;
-    int inOrder[],postOrder[],position[];
-    public Solution() {
-        Scanner scan = new Scanner(System.in);
-        n = scan.nextInt();
-        inOrder = new int[n+1];
-        postOrder = new int[n+1];
-        position = new int[n+1];
-        for(int i=0;i<n;i++) {
-            inOrder[i] = scan.nextInt();
-        }
-        for(int i=0;i<n;i++) {
-            postOrder[i] = scan.nextInt();
-        }
-        for(int i=0;i<n;i++) {
-            position[inOrder[i]] = i; // 인오더에서 루트의 위치를 저장해주는 인덱스배열
-        }
-        solve(0,n-1,0,n-1);
-    }
-    public void solve(int inStart, int inEnd, int postStart, int postEnd) {
-        if(inStart > inEnd || postStart > postEnd) return;
-        int root = postOrder[postEnd]; // 포스트오더의 끝부분이 루트이다
-        System.out.print(root+" ");
-        int p = position[root];
-        solve(inStart, p-1, postStart, postStart+(p-inStart)-1); // 왼쪽탐색
-        solve(p+1,inEnd,postStart+(p-inStart),postEnd-1); // 오른쪽탐색
-    }
+	static int[] inOrder;
+	static int[] inOrderIdx;
+	static int[] postOrder;
+	
+	public static void main(String[] args) throws IOException {
+		
+		/* 입력 및 변수 초기화 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		inOrder = new int[n+1];
+		inOrderIdx = new int[n+1];
+		postOrder = new int[n+1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			inOrder[i] = Integer.parseInt(st.nextToken());
+			inOrderIdx[inOrder[i]] = i;
+		}
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			postOrder[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		getPreOrder(0, n, 0, n); // inOrder의 시작 idx, inOrder의 끝idx, postOrder의 시작 idx, postOrder의 끝 idx
+	}
+
+	/* recursive : 인오더와 포스터오더로 프리오더 구하기 */
+	private static void getPreOrder(int startI, int endI, int startP, int endP) {
+		if(startI >= endI || startP >= endP) return;
+		int p = postOrder[endP-1];
+		int index = inOrderIdx[p];
+		System.out.print(p + " ");
+		getPreOrder(startI, index, startP, startP+(index-startI));
+		getPreOrder(index+1, endI, index-startI+startP, endP-1);
+	}
 }
