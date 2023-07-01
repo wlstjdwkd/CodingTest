5d2132f22320d9f544701b644b2e4de48bf72059
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2023-07-01 20:00:21 +0900

    [Gold IV] Title: 전화번호 목록, Time: 596 ms, Memory: 32156 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            String[] phone_number = new String[N]; // 전화번호 목록.
+
+            for (int i = 0; i < N; i++) {
+                phone_number[i] = br.readLine();
+            }
+            Arrays.sort(phone_number); // 오름차순으로 정렬.
+
+            if (isConsistent(N, phone_number)) {
+                sb.append("YES").append("\n");
+            } else {
+                sb.append("NO").append("\n");
+            }
+        }
+        System.out.println(sb);
+
+    }
+
+    // 전화번호 목록이 일관성이 있는지 검사.
+    public static boolean isConsistent(int N, String[] phone_number) {
+        // 현재 배열은 오름차순 정렬이 되어있는 상태.
+        // 만약 전화 번호 목록 안에 접두어 관계가 있는 문자열이 있다면,
+        // 특정 문자열 바로 뒤에 접두어 관계가 있는 문자열이 있을 것이다.
+        // 결과적으로, 전화번호 목록이 일관성이 있는지 검사하려면
+        // 특정 문자열과 그 다음 문자열의 접두어 관계만 확인하면 된다.
+        for (int i = 0; i < N - 1; i++) {
+            if (phone_number[i + 1].startsWith(phone_number[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
