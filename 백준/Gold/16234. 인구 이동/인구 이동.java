e0c05c093ac92526383c1966b14e39a51ee30dd3
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2023-07-24 15:58:57 +0900

    [Gold V] Title: 인구 이동, Time: 540 ms, Memory: 294144 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,117 +1,105 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, l, r;
-    static int[][] map;
+    static int N,L,R;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int[][] arr;
     static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static ArrayList<Nation> unitedNations; // 같은 연합인 나라들
-    static class Nation {
-        int x, y, population;
-        public Nation(int x, int y) {
-            this.x = x;
-            this.y = y;
-            this.population = map[x][y];
-        }
-    }
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    static ArrayList<Node> list;
 
     public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
 
-        /* 입력 */
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        l = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        map = new int[n][n];
-
-        for(int i=0; i<n; i++) {
+        arr = new int[N][N];
+        for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=0; j<N; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-
-        /* 실행 */
-        System.out.println(getMovingPeriod());
-
+        System.out.println(move());
     }
-
-    /* 이동하는 날짜 구하기 */
-    private static int getMovingPeriod() {
-        int day = 0;
-        while(true) {
-            boolean isMoved = false;
-            visited = new boolean[n][n];
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<n; j++) {
-                    if(visited[i][j]) continue;
-                    int sum = searchPopulation(i, j);
-                    if(unitedNations.size() > 1) {
-                        unionNations(sum);
-                        isMoved = true;
+    public static int move(){
+        int answer = 0;
+        while(true){
+            boolean isMove = false;
+            visited = new boolean[N][N];
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    if(!visited[i][j]){
+                        int sum = bfs(i,j);
+                        if(list.size() > 1){
+                            changePolulation(sum);
+                            isMove = true;
+                        }
                     }
                 }
             }
-            if(!isMoved) return day;
-            day++;
+            if(!isMove){
+                return answer;
+            }
+            answer++;
         }
     }
 
+    public static int bfs(int x, int y){
+        Queue<Node> q = new ArrayDeque<>();
+        list = new ArrayList<>();
 
-    /* bfs : 인접한 나라끼리 연합할 수 있는지 탐색 */
-    private static int searchPopulation(int startX, int startY) {
-        unitedNations = new ArrayList<>();
-        Deque<Nation> queue = new ArrayDeque<>();
-        Nation nation = new Nation(startX, startY);
-        queue.offer(nation);
-        unitedNations.add(nation);
-        visited[nation.x][nation.y] = true;
-        int sum = nation.population;
+        q.add(new Node(x,y));
+        list.add(new Node(x,y));
+        visited[x][y] = true;
+        int sum = arr[x][y];
+        while(!q.isEmpty()){
+            Node current = q.poll();
 
-        while(!queue.isEmpty()) {
-            nation = queue.poll();
-
-
-//            System.out.println("여기 : " + nation.population  + " " + nation.x + " " + nation.y);
-
-            for(int i=0; i<4; i++) {
-
-                int nx = nation.x + dx[i];
-                int ny = nation.y + dy[i];
-
-                if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                if(visited[nx][ny]) continue;
-                if(!checkPopulation(nation, map[nx][ny])) continue;
-
-                Nation newNation = new Nation(nx, ny);
-                sum += newNation.population;
-                unitedNations.add(newNation);
-                queue.offer(newNation);
-                visited[newNation.x][newNation.y] = true;
-
+            for(int i=0; i<4; i++){
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
+                if(isInArea(nx,ny)&&!visited[nx][ny]){
+                    int diff = Math.abs(arr[current.x][current.y]-arr[nx][ny]);
+                    if(L <= diff && diff <= R){
+                        q.add(new Node(nx,ny));
+                        list.add(new Node(nx,ny));
+                        sum += arr[nx][ny];
+                        visited[nx][ny]= true;
+                    }
+                }
             }
         }
-
         return sum;
     }
 
-    /* 두 나라의 인구 차를 확인  */
-    private static boolean checkPopulation(Nation nation, int population) {
-        int difference = Math.abs(nation.population - population);
-        return difference >= l && difference <= r ;
+    public static boolean isInArea(int x, int y){
+        return 0<= x && x< N && 0<= y && y<N;
     }
 
-    /* 국경선 열린 나라끼리 연합 */
-    private static void unionNations(int sum) {
-        int average = sum / unitedNations.size();
-        for(Nation nation : unitedNations) {
-            map[nation.x][nation.y] = average;
+    public static void changePolulation(int sum){
+        int avg = sum/list.size();
+        for(Node n : list){
+            arr[n.x][n.y] = avg;
         }
     }
 
+    public static class Node{
+        int x,y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
