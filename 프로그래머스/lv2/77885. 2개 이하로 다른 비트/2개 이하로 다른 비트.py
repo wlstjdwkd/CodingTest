285dd2ff5d097b28fda731707769e3d0376bf077
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2023-08-21 22:24:29 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 163.10 ms, Memory: 23.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -0,0 +1,15 @@
+def solution(numbers):
+    answer = []
+    for i in numbers:
+        if i%2 ==0:
+            answer.append(i+1)
+        else:
+            a=bin(i)[2:]
+            a="0"+a
+            one_idx=a.rfind("0")
+            a=list(a)
+            a[one_idx]="1"
+            a[one_idx+1]="0"
+            answer.append(int("".join(a),2))
+
+    return answer
