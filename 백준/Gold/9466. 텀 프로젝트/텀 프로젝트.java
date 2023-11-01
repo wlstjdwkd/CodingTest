96a3e34d7794c917f39b389557e8d7e2ac8de923
wlstjdwkd <74286424+wlstjdwkd@users.noreply.github.com>
2023-11-01 22:03:14 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1036 ms, Memory: 302740 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -4,59 +4,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    private static int n;
-    private static int[] arr;
-    private static int count=0;
-    private static boolean[] visited;
-    private static boolean[] finished;
-
+    static int[] arr;
+    static boolean[] visited;
+    static boolean[] finished;
+    static int result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        int T= Integer.parseInt(br.readLine());
-        while(T-->0){
-            n = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        while(T -- > 0){
+            int n = Integer.parseInt(br.readLine());
+            arr = new int[n+1];
             visited = new boolean[n+1];
             finished = new boolean[n+1];
-            count = 0;
-            arr=new int[n+1];
 
-            for(int i=1; i<n+1; i++){
+            result = 0;
+            st = new StringTokenizer(br.readLine());
+            for(int i=1; i<=n; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
             }
-
-            for(int i=1; i<n+1; i++){
-                dfs(i);
-            }
-
-            System.out.println(n-count);
-        }
-
-    }
-
-    private static void dfs(int now){
-        if(visited[now]){
-            return;
-        }
-
-        visited[now] = true;
-        int next = arr[now];
-
-        if(visited[next] != true){
-            dfs(next);
-        }
-        else{
-            if(finished[next] != true){
-                count++;
-                for(int i=next; i != now; i = arr[i]){
-                    count++;
+            for(int i=1; i<=n; i++){
+                if(!finished[i]){
+                    dfs(i);
                 }
             }
+            sb.append(n-result).append("\n");
         }
+        System.out.println(sb);
+    }
+    public static void dfs(int cur){
+        if(finished[cur]) return;
+        if(visited[cur]){
+            finished[cur] = true;
+            result++;
+        }
+        visited[cur] = true;
+        dfs(arr[cur]);
+        finished[cur] = true;
+//        visited[cur] = false;
 
-        finished[now] = true;
+
     }
 }
