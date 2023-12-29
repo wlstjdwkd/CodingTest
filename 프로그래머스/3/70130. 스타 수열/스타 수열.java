2814844e299b9717696ad1a56e541740ebe058ba
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2023-12-29 11:32:09 +0900

    [level 3] Title: 스타 수열, Time: 0.05 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] a) {
+        int answer = 0;
+        
+        int[] cnt = new int[a.length];
+        for(int i=0; i<a.length; i++){
+            cnt[a[i]]++;
+        }
+        
+        for(int i=0; i<cnt.length; i++){
+            if(cnt[i]>answer){
+                int ans = 0;
+                for(int j=0; j<a.length-1; j++){
+                    if(a[j]!=a[j+1] && (i==a[j] || i==a[j+1])){
+                        ans++;
+                        j++;
+                    }
+                }
+                answer = Math.max(answer, ans);
+                
+            }
+        }
+        
+        return answer*2;
+    }
+}
