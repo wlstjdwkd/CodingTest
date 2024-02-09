b885522e29a0ff1221006574a15035590c9feba6
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-02-09 12:57:01 +0900

    [level 3] Title: 가장 먼 노드, Time: 34.50 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    public int solution(int n, int[][] edge) {
+        for(int i=0; i<n+1; i++){
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] x : edge ){
+            graph.get(x[0]).add(x[1]);
+            graph.get(x[1]).add(x[0]);
+        }
+        
+        boolean[] visited = new boolean[n+1];
+        int answer = bfs(graph, visited, n);
+        
+        return answer;
+    }
+    
+    public int bfs(ArrayList<ArrayList<Integer>> graph, boolean[] visited, int n){
+        Queue<Integer> q = new ArrayDeque<>();
+        int[] depth = new int[n+1];
+        q.add(1);
+        visited[1] = true;
+        depth[1] = 1;
+        while(!q.isEmpty()){
+            int x = q.poll();
+            for(int i=0; i<graph.get(x).size(); i++){
+                int next = graph.get(x).get(i);
+                if(!visited[next]){
+                    visited[next] = true;
+                    depth[next] += depth[x]+1;
+                    q.add(next);
+                }
+            }
+        }
+        
+        int m = Arrays.stream(depth).max().getAsInt();
+        int answer = 0;
+        for(int x : depth){
+            if(x == m){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
