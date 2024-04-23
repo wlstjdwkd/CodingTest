0dfb413926f91c0d3f6fb20e50a7f8d4cd5ba335
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-04-23 09:15:15 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 608 ms, Memory: 59220 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,28 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Time;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] array;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int n=Integer.parseInt(st.nextToken());
-        int m=Integer.parseInt(st.nextToken());
 
-        st=new StringTokenizer(br.readLine());
-        array=new int[n+1];
-        for(int i=1;i<=n;i++) { // i까지의 누적합 구하기
-            array[i]=array[i-1]+Integer.parseInt(st.nextToken());
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N+1];
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        arr[0]= 0;
+        for(int i=1; i<=N; i++){
+          arr[i] = arr[i-1]+Integer.parseInt(st.nextToken());
         }
 
-        for(int i=0;i<m;i++) {
-            st=new StringTokenizer(br.readLine());
-            int a=Integer.parseInt(st.nextToken());
-            int b=Integer.parseInt(st.nextToken());
-            // a, b사이의 구간합은 array[b]-array[a-1]과 같다
-            System.out.println(array[b]-array[a-1]);
+        for(int i=0; i<M; i++){
+          st = new StringTokenizer(br.readLine());
+          int start = Integer.parseInt(st.nextToken());
+          int end = Integer.parseInt(st.nextToken());
+          sb.append(arr[end] - arr[start-1]).append("\n");
         }
+      System.out.println(sb);
     }
 }
