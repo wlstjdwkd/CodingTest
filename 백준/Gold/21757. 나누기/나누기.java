b073e1daf35a8b01e236429ddac3afc59e817282
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-06-24 17:17:29 +0900

    [Gold II] Title: 나누기, Time: 248 ms, Memory: 29164 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        int N = stoi(br.readLine());
+        long[] num = new long[N+1];
+        long[] sum = new long[N+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++){
+            num[i] = Long.parseLong(st.nextToken());
+            sum[i] = sum[i-1] + num[i];
+        }
+
+        long[][] dp = new long[N+1][4];
+        long count = 0;
+        if(sum[N] % 4 == 0){
+            long target = sum[N]/4;
+
+            dp[0][0] = 1;
+
+            for(int i = 1; i < N; i++){
+                dp[i][0] = 1;
+                for(int j=1; j<=3; j++){
+                    dp[i][j] = dp[i-1][j];
+
+                    if(sum[i] == target * j){
+                        dp[i][j] += dp[i-1][j-1];
+                    }
+                }
+            }
+
+            count = dp[N-1][3];
+        }
+
+        System.out.println(count);
+
+    }
+}
