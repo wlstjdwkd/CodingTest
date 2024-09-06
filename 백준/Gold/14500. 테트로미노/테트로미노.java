c1bc9a411633a8e12098bb09a9cc5f497054bf00
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-09-06 09:33:07 +0900

    [Gold IV] Title: 테트로미노, Time: 636 ms, Memory: 32416 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,69 +1,88 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int N,M;
-    static int[][] map;
-    static boolean[][] visit;
-    static int max = Integer.MIN_VALUE;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static class Point{
+        int x, y;
+        Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private static boolean isArea(int x, int y){
+        return 0<= x && x<n && 0<=y && y<m;
+    }
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M =Integer.parseInt(st.nextToken());
+        n = stoi(st.nextToken());
+        m = stoi(st.nextToken());
+        arr = new int[n][m];
+        visit = new boolean[n][m];
 
-        map = new int[N][M];
-        visit = new boolean[N][M];
-        for(int i=0; i<N; i++){
+        for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<M; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=0;j<m;j++){
+                arr[i][j] = stoi(st.nextToken());
             }
         }
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 visit[i][j] = true;
-                dfs(i,j,map[i][j],1);
+                solve(i,j,arr[i][j],1);
                 visit[i][j] = false;
             }
         }
+
         System.out.println(max);
 
     }
-    private static void dfs(int row, int col, int sum, int count){
-        if(count ==4){
-            max = Math.max(sum,max);
+
+    private static void solve(int row, int col, int sum, int count){
+        if(count == 4){
+            max = Math.max(max, sum);
             return;
         }
 
         for(int i=0; i<4; i++){
-            int nr = row + dx[i];
-            int nc = col + dy[i];
+            int curRow = row + dx[i];
+            int curCol = col + dy[i];
 
-            if(isInArea(nr,nc)){
-                if(!visit[nr][nc]){
-                    if(count == 2) {
-                        visit[nr][nc] = true;
-                        dfs(row, col, sum + map[nr][nc], count + 1);
-                        visit[nr][nc] = false;
+            if(isArea(curRow, curCol)){
+                if(!visit[curRow][curCol]){
+                    if(count == 2){
+                        visit[curRow][curCol] = true;
+                        solve(row, col, sum + arr[curRow][curCol], count + 1);
+                        visit[curRow][curCol] = false;
                     }
-
-                    visit[nr][nc] = true;
-                    dfs(nr, nc, sum + map[nr][nc], count + 1);
-                    visit[nr][nc] = false;
+                    visit[curRow][curCol] = true;
+                    solve(curRow, curCol, sum + arr[curRow][curCol], count + 1);
+                    visit[curRow][curCol] = false;
                 }
             }
         }
     }
-    private static boolean isInArea(int x, int y){
-        return 0<=x && x<N && 0<=y && y<M;
-    }
+
+    private static int max = Integer.MIN_VALUE;
+    private static int[][] arr;
+    private static boolean[][] visit;
+    private static int n,m;
+
+    private static int[] dx = {-1,1,0,0};
+    private static int[] dy = {0,0,-1,1};
+
+
+
 }
