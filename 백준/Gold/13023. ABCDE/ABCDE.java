a51cd8e027bc36d938c11801544f5ca6ebaec53c
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-20 17:33:06 +0900

    [Gold V] Title: ABCDE, Time: 172 ms, Memory: 20452 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,65 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 
-    static boolean status;
-    static List<Integer>[] list;
-    static boolean[] check;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        list = new ArrayList[n+1];
-        for(int i=0; i<n; i++){
-            list[i] = new ArrayList<>();
-        }
-
-        for(int i=0; i<m; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            list[a].add(b);
-            list[b].add(a);
-        }
-
-        status = false;
-        for(int i=0; i<n; i++){
-            check = new boolean[n];
-            dfs(i,1);
-            if(status){
-                System.out.println(1);
-                return;
-
-            }
-        }
-        System.out.println(0);
+    private static int stoi(String s){
+        return Integer.parseInt(s);
     }
 
-    private static void dfs(int idx, int depth){
+    private static int N,M;
+    private static boolean[] visited;
+    private static List<Integer>[] list;
+    private static int ans;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+        list = new ArrayList[N];
+        for(int i=0; i<N; i++){
+            list[i] = new ArrayList<>();
+        }
+        visited = new boolean[N];
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = stoi(st.nextToken());
+            int b = stoi(st.nextToken());
+
+            list[a].add(b);
+            list[b].add(a);
+
+        }
+
+        for(int i=0; i<N; i++){
+            if(ans == 0){
+                dfs(i,1);
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    private static void dfs(int start, int depth){
         if(depth == 5){
-            status = true;
+            ans = 1;
             return;
         }
 
-        check[idx] = true;
-        for(int nxt : list[idx]){
-            if(!check[nxt]){
-                dfs(nxt, depth+1);
+        visited[start] = true;
+        for(int i : list[start]){
+            if(!visited[i]){
+                dfs(i, depth+1);
             }
         }
-        
-        check[idx] = false;
+        visited[start] = false;
     }
+
 }
