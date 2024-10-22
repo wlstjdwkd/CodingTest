5dce3f25c39a33ebff2f0d7b730219f2b858c859
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-23 00:40:15 +0900

    [Gold III] Title: 내리막 길, Time: 272 ms, Memory: 38576 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,62 +1,76 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx ={0,-1,1,0};
-    static int[] dy = {1,0,0,-1};
-    static int[][] arr;
-    static int[][] dp;
-    static int M,N,answer;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 
-        arr = new int[M][N];
-        dp = new int[M][N];
 
-        answer=0;
-        for(int i=0; i<M; i++){
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static final int[] dx = {-1,0,1,0};
+    private static final int[] dy = {0,1,0,-1};
+
+    private static boolean isArea(int x, int y){
+        return 1<=x && x<=m && 1<=y && y<=n;
+    }
+
+    private static int n,m;
+    private static int[][] dp;
+    private static int[][] map;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+
+        m = stoi(st.nextToken());
+        n = stoi(st.nextToken());
+
+        map = new int[m+1][n+1];
+        dp = new int[m+1][n+1];
+
+        for(int i=1; i<=m; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j]=-1;
+            for(int j=1; j<=n; j++){
+                map[i][j] = stoi(st.nextToken());
+                dp[i][j] = -1;
             }
         }
 
-
-        System.out.println(dfs(0,0));
+        System.out.println(dfs(1,1));
 
     }
-    public static int dfs(int x,int y){
-        if(x==N-1 && y==M-1){
+
+    private static int dfs(int x, int y){
+        if(x == m && y == n){
             return 1;
         }
-        if(dp[y][x]!=-1){
-            return dp[y][x];
+
+        if(dp[x][y] != -1){
+            return dp[x][y];
         }
-        dp[y][x]=0;
+
+        dp[x][y] = 0;
         for(int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            int score=arr[y][x];
-
-            if(isInArea(nx,ny)){
-                if(score>arr[ny][nx]){
-
-                    dp[y][x]+=dfs(nx,ny);
+            if(isArea(nx, ny)){
+                if(map[x][y] > map[nx][ny]){
+                    dp[x][y] += dfs(nx, ny);
                 }
             }
         }
 
-
-        return dp[y][x];
-
+        return dp[x][y];
     }
-    public static boolean isInArea(int x, int y){
-        return 0<=x && x<N && 0<=y && y<M;
-    }
+
+
+
 }
