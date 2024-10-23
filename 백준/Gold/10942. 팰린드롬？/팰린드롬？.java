3374ca8447ffeb153e2fdbccc5e40ba69e854f82
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-23 18:23:07 +0900

    [Gold IV] Title: 팰린드롬?, Time: 616 ms, Memory: 230932 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,55 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[][] dp;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N+1];
-        dp = new boolean[N+1][N+1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static final int[] dx = {-1,0,1,0};
+    private static final int[] dy = {0,1,0,-1};
+
+//    private static boolean isArea(int x, int y){
+//        return 1<=x && x<=m && 1<=y && y<=n;
+//    }
+
+    private static boolean[][] dp;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+
+        int n = stoi(st.nextToken());
+        int[] arr = new int[n+1];
+        dp = new boolean[n+1][n+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=n; i++){
+            arr[i] = stoi(st.nextToken());
         }
 
-        solve(arr, N);
-        int m = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
+        solve(arr,n);
+        int m = stoi(br.readLine());
         for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            if(dp[s][e]){
-                sb.append(1).append("\n");
-            }else{
-                sb.append(0).append("\n");
+            int start = stoi(st.nextToken());
+            int end = stoi(st.nextToken());
+
+            if(dp[end][start]){
+                sb.append("1\n");
+            }
+            else{
+                sb.append("0\n");
             }
         }
-        System.out.println(sb.toString());
+
+        System.out.println(sb);
+
     }
-    public static void solve(int[] arr, int n){
+
+    private static void solve(int[] arr, int n){
         for(int i=1; i<=n; i++){
             dp[i][i] = true;
         }
 
-        for(int i=1; i<=n-1; i++){
+        for(int i=1; i<n; i++){
             if(arr[i] == arr[i+1]){
-                dp[i][i+1] = true;
+                dp[i+1][i] = true;
             }
         }
 
-        for(int i=2; i<n; i++){
-            for(int j=1; j<=n-i; j++){
-                if(arr[j] == arr[j+i] && dp[j+1][j+i-1]){
-                    dp[j][j+i] = true;
+        for(int i=3; i<=n; i++){
+            for(int j=1; j<=n-2; j++){
+                if(dp[i-1][j+1] && arr[i] == arr[j]){
+                    dp[i][j] = true;
                 }
             }
         }
     }
+
+
+
 }
