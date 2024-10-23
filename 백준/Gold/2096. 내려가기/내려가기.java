3d3b51f8a151a0171406735dccfdcaf13392f417
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-23 23:33:24 +0900

    [Gold V] Title: 내려가기, Time: 276 ms, Memory: 40108 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,47 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr= new int[N][3];
-        int[] maxDp = new int[3];
-        int[] minDp = new int[3];
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static final int[] dx = {-1,0,1,0};
+    private static final int[] dy = {0,1,0,-1};
+
+//    private static boolean isArea(int x, int y){
+//        return 1<=x && x<=m && 1<=y && y<=n;
+//    }
+
+
+
+    public static void main(String[] args) throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = stoi(st.nextToken());
+
+        int[] maxDP = new int[3];
+        int[] minDP = new int[3];
+
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int x3 = Integer.parseInt(st.nextToken());
 
-            if(i==0){
-                maxDp[0]=minDp[0]=x1;
-                maxDp[1]=minDp[1]=x2;
-                maxDp[2]=minDp[2]=x3;
+            int x1 = stoi(st.nextToken());
+            int x2 = stoi(st.nextToken());
+            int x3 = stoi(st.nextToken());
 
+            if(i == 0){
+                maxDP[0] = minDP[0] = x1;
+                maxDP[1] = minDP[1] = x2;
+                maxDP[2] = minDP[2] = x3;
             }
             else{
-                //최댓값
-                int beforeMaxDp0 = maxDp[0], beforeMaxDp2=maxDp[2];
-                maxDp[0] = Math.max(maxDp[0], maxDp[1]) + x1;
-                maxDp[2] = Math.max(maxDp[1], maxDp[2]) + x3;
-                maxDp[1] = Math.max(Math.max(beforeMaxDp0,maxDp[1]), beforeMaxDp2)+x2;
+                int beforeMaxDP_0 = maxDP[0];
+                int beforeMaxDP_2 = maxDP[2];
 
-                //최솟값
-                int beforeMinDp_0 = minDp[0], beforeMinDp_2 = minDp[2];
-                minDp[0] = Math.min(minDp[0], minDp[1]) + x1;
-                minDp[2] = Math.min(minDp[1], minDp[2]) + x3;
-                minDp[1] = Math.min(Math.min(beforeMinDp_0, minDp[1]), beforeMinDp_2) + x2;
+                maxDP[0] = Math.max(maxDP[0], maxDP[1]) + x1;
+                maxDP[2] = Math.max(maxDP[1], maxDP[2]) + x3;
+                maxDP[1] = Math.max(Math.max(beforeMaxDP_0, maxDP[1]), beforeMaxDP_2) + x2;
+
+                int beforeMinDp_0 = minDP[0], beforeMinDp_2 = minDP[2];
+                minDP[0] = Math.min(minDP[0], minDP[1]) + x1;
+                minDP[2] = Math.min(minDP[1], minDP[2]) + x3;
+                minDP[1] = Math.min(Math.min(beforeMinDp_0, minDP[1]), beforeMinDp_2) + x2;
+
+
             }
+
+
+
         }
-        sb.append(Math.max(Math.max(maxDp[1],maxDp[2]),maxDp[0])).append(" ")
-                .append(Math.min(Math.min(minDp[1],minDp[2]),minDp[0]));
 
-        System.out.println(sb);
-
+        System.out.print(Math.max(Math.max(maxDP[0], maxDP[1]), maxDP[2]) + " ");
+        System.out.println(Math.min(Math.min(minDP[0], minDP[1]), minDP[2]));
     }
+
 }
