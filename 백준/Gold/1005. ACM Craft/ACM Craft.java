bbf6a7cf8f4466683b0a0ef5be2ccb1fce4af93a
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-23 16:27:25 +0900

    [Gold III] Title: ACM Craft, Time: 680 ms, Memory: 243948 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,73 +1,101 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static int N,K;
-    private static int[] delay;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
     private static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        while(T -- > 0){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-            delay = new int[N+1];
 
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static final int[] dx = {-1,0,1,0};
+    private static final int[] dy = {0,1,0,-1};
+
+//    private static boolean isArea(int x, int y){
+//        return 1<=x && x<=m && 1<=y && y<=n;
+//    }
+
+    private static int n;
+    private static int k;
+    private static int[] d;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+
+        int tc = stoi(st.nextToken());
+
+        for(int t = 0; t < tc; t++){
             st = new StringTokenizer(br.readLine());
-            for(int i=1; i<=N; i++){
-                delay[i] = Integer.parseInt(st.nextToken());
-            }
+            n = stoi(st.nextToken());
+            k = stoi(st.nextToken());
+            d = new int[n+1];
+
             List<List<Integer>> list = new ArrayList<>();
-            for(int i=0; i<N+1; i++){
+
+            for(int i=0; i<n+1; i++){
                 list.add(new ArrayList<>());
             }
-            int[] indegree = new int[N+1];
 
-            while(K -- > 0){
+            int[] degree = new int[n+1];
+
+            st = new StringTokenizer(br.readLine());
+            for(int i=1; i<=n; i++){
+                d[i] = stoi(st.nextToken());
+            }
+
+            for(int i=0; i<k; i++){
                 st = new StringTokenizer(br.readLine());
-                // v1 -> v2
-                int v1 = Integer.parseInt(st.nextToken());
-                int v2 = Integer.parseInt(st.nextToken());
+
+                int v1 = stoi(st.nextToken());
+                int v2 = stoi(st.nextToken());
 
                 list.get(v1).add(v2);
-                indegree[v2]++;
+                degree[v2]++;
             }
-            int w = Integer.parseInt(br.readLine());
 
-            topologicalSort(indegree, list, w);
+            int w = stoi(br.readLine());
 
+            topoSort(degree, list, w);
         }
-        System.out.println(sb);
     }
 
-    private static void topologicalSort(int[] indegree, List<List<Integer>> list, int w) {
+    private static void topoSort(int[] degree, List<List<Integer>> list, int w){
         Queue<Integer> q = new ArrayDeque<>();
-        int[] result = new int[N+1];
+        int[] result = new int[n+1];
 
-        // 건물의 소요 기본 소요시간은 d[i]
-        for(int i=1; i<=N; i++) {
-            result[i] = delay[i];
+        for(int i=1; i<=n; i++){
+            result[i] = d[i];
 
-            if(indegree[i] == 0)
-                q.offer(i);
-        }
-
-        // 건물의 총 소요시간 = 이전까지의 소요시간 + 현재 건물 소요시간
-        // Max 해주는 이유는 이전 테크가 다 올라야 현재 건물을 지을 수 있기 때문
-        while(!q.isEmpty()) {
-            int node = q.poll();
-
-            for(Integer i : list.get(node)) {
-                result[i] = Math.max(result[i], result[node] + delay[i]);
-                indegree[i]--;
-
-                if(indegree[i] == 0)
-                    q.offer(i);
+            if(degree[i] == 0){
+                q.add(i);
             }
         }
-        sb.append(result[w]).append("\n");
+
+        while(!q.isEmpty()){
+            int v = q.poll();
+            for (final int i : list.get(v)) {
+                degree[i]--;
+                result[i] = Math.max(result[i], result[v] + d[i]);
+
+                if(degree[i] == 0){
+                    q.add(i);
+                }
+            }
+        }
+
+        System.out.println(result[w]);
     }
+
+
+
 }
