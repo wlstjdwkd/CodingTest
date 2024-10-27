47003546993197a814d9f22527cdc05a74ab3d82
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-27 19:21:21 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 240 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -1,22 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, INF = Integer.MAX_VALUE;
-    static int[] data;
-    static int[][] dp;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static int n, INF = Integer.MAX_VALUE;
+    private static int[] data;
+    private static int[][] dp;
+
+//    private static boolean isArea(int x, int y){
+//        return 1<=x && x<=N && 1<=y && y<=N;
+//    }
+
 
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = stoi(st.nextToken());
         data = new int[N+1];
-        for(int i=0;i<N; i++){
+        for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            int r = stoi(st.nextToken());
+            int c = stoi(st.nextToken());
             data[i] = r;
             data[i+1] = c;
         }
@@ -25,20 +46,23 @@ public class Main {
         for(int i=0; i<N; i++){
             Arrays.fill(dp[i], INF);
         }
-        System.out.println(solve(0, N-1));
-
+        System.out.println(solve(0,N-1));
     }
-    static int solve(int pos, int cur){
+
+    private static int solve(int pos, int cur){
         if(pos == cur){
             return 0;
         }
-        if(dp[pos][cur] != INF){
-            return dp[pos][cur];
+
+        if(dp[pos][cur] != INF) return dp[pos][cur];
+
+        for(int i= pos; i<cur; i++){
+            int value = solve(pos, i) + solve(i+1, cur) +data[pos] * data[i+1] * data[cur+1];
+            dp[pos][cur] = Math.min(value, dp[pos][cur]);
         }
-        for(int i=pos; i<cur; i++){
-            int value = solve(pos, i) + solve(i+1, cur) + data[pos] * data[i+1] * data[cur+1];
-            dp[pos][cur] = Math.min(dp[pos][cur], value);
-        }
+
         return dp[pos][cur];
     }
+
+
 }
