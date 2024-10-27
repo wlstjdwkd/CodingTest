cb80f3b72178902d5acbb7ffca67fda19ff2be6a
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-28 01:29:21 +0900

    [Gold III] Title: 파일 합치기, Time: 724 ms, Memory: 18972 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,36 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+
+
+//    private static boolean isArea(int x, int y){
+//        return 1<=x && x<=N && 1<=y && y<=N;
+//    }
+    private static int[] novel;
+    private static int[][] dp;
+    private static int[] sum;
+
+    public static void main(String[] args) throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+
+        int T = stoi(st.nextToken());
         while(T-->0){
-            int K = Integer.parseInt(br.readLine());
+            int K = stoi(br.readLine());
             st = new StringTokenizer(br.readLine());
-            int[] novel = new int[K+1];
-            int[] sum = new int[K+1];
-            int[][] dp = new int[K+1][K+1];
+            novel = new int[K+1];
+            dp = new int[K+1][K+1];
+            sum = new int[K+1];
+
+            for(int i=0; i<K+1; i++){
+                Arrays.fill(dp[i], Integer.MAX_VALUE);
+            }
 
             for(int i=1; i<=K; i++){
-                novel[i] = Integer.parseInt(st.nextToken());
+                novel[i] = stoi(st.nextToken());
                 sum[i] = sum[i-1] + novel[i];
             }
 
-            for(int n=1; n<=K; n++){
-                for(int from = 1; from+n <=K; from++){
-                    int to = from+n;
-                    dp[from][to] = Integer.MAX_VALUE;
-                    for(int divide = from; divide<to; divide++){
-                        dp[from][to] = Math.min(dp[from][to], dp[from][divide] + dp[divide+1][to] + sum[to] -sum[from-1]);
-                    }
-                }
-            }
+            sb.append(solve(1, K)).append('\n');
 
-            System.out.println(dp[1][K]);
         }
+        System.out.println(sb);
     }
+
+    private static int solve(int prev, int last){
+        if(prev == last){
+            return 0;
+        }
+        if(dp[prev][last] != Integer.MAX_VALUE){
+            return dp[prev][last];
+        }
+
+        for(int i=prev; i<last; i++){
+            int value = solve(prev, i) + solve(i+1, last) + sum[last] - sum[prev-1];
+            dp[prev][last] = Math.min(dp[prev][last], value);
+        }
+
+        return dp[prev][last];
+    }
+
+
 }
