0e4114bbdc843c7dabac918263d32ca3cd1e5b62
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-28 09:44:55 +0900

    [Gold V] Title: 퇴사 2, Time: 744 ms, Memory: 343244 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,42 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+
 
     private static int stoi(String s){
         return Integer.parseInt(s);
     }
 
 
-    public static void main(String[] args) throws IOException {
-        int n = stoi(br.readLine());
-        int[][] arr = new int[n+2][2];
-        int[] dp = new int[n+2];
-        for(int i=1; i<=n; i++){
-            st = new StringTokenizer(br.readLine());
 
-            int t = stoi(st.nextToken());
-            int p = stoi(st.nextToken());
-            arr[i][0] = t;
-            arr[i][1] = p;
+//    private static boolean isArea(int x, int y){
+//        return 1<=x && x<=N && 1<=y && y<=N;
+//    }
+    private static int[] novel;
+    private static int[][] dp;
+    private static int[] sum;
+
+    public static void main(String[] args) throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = stoi(st.nextToken());
+
+        int[][] arr = new int[N+2][2];
+        int[] dp = new int[N+2];
+
+        for(int i= 1; i<=N; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = stoi(st.nextToken());
+            arr[i][1] = stoi(st.nextToken());
         }
 
         int max = -1;
-        for(int i=1; i<=n+1; i++){
+        for(int i=1; i<=N+1; i++){
             if(max < dp[i]){
                 max = dp[i];
             }
 
             int nxt = i + arr[i][0];
-            if(nxt < n+2){
+            if(nxt<N+2){
                 dp[nxt] = Math.max(dp[nxt], max + arr[i][1]);
             }
         }
 
-        System.out.println(dp[n+1]);
+        System.out.println(dp[N+1]);
     }
+
+
 }
