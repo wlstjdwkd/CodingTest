abbcf550acd8ee14b439ec2264fca7558daef8d4
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-29 14:11:24 +0900

    [Gold IV] Title: RGB거리 2, Time: 84 ms, Memory: 12132 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -1,49 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr= new int[N+1][3];
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1,0};
+
+//    private static boolean isArea(int x, int y){
+//        return 0<=x && x<m && 0<=y && y<n;
+//    }
+
+
+
+
+    public static void main(String[] args) throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = stoi(st.nextToken());
+        int[][] arr = new int[N+1][3];
         int[][] dp = new int[N+1][3];
 
-        int INF = 1_000 * 1_000;
-        int answer = INF;
-
         for(int i=1; i<=N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for(int j=0; j<3; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = stoi(st.nextToken());
             }
         }
 
-        for(int k=0; k<3; k++){
-            for(int i=0; i<3; i++){
-                if(i==k){
-                    dp[1][i] = arr[1][i];
+        final int INF = 1_000 * 1_000;
+
+        int min = INF;
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                if(i == j){
+                    dp[1][j] = arr[1][j];
                 }
                 else{
-                    dp[1][i] = INF;
+                    dp[1][j] = INF;
                 }
             }
 
-            for(int i=2; i<=N; i++){
-                dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + arr[i][0];
-                dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + arr[i][1];
-                dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + arr[i][2];
+            for(int j=2; j<=N; j++){
+                dp[j][0] = Math.min(dp[j-1][1], dp[j-1][2]) + arr[j][0];
+                dp[j][1] = Math.min(dp[j-1][0], dp[j-1][2]) + arr[j][1];
+                dp[j][2] = Math.min(dp[j-1][0], dp[j-1][1]) + arr[j][2];
             }
 
-            for(int i=0; i<3; i++){
-                if(i==k){
-                    continue;
+            for(int j=0; j<3; j++){
+                if(i!=j){
+                    min = Math.min(min, dp[N][j]);
                 }
-                answer = Math.min(answer, dp[N][i]);
             }
         }
+        System.out.println(min);
 
-        System.out.println(answer);
     }
+
+
+
 }
