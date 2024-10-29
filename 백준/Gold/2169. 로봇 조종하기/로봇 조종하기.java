99143c6cbe07220ca0fe0d48a3d69136bd6b016f
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-29 15:24:51 +0900

    [Gold II] Title: 로봇 조종하기, Time: 492 ms, Memory: 72356 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -1,49 +1,79 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M;
-    static int[][] mars;
-    static int[][] dp;
-    static int[][] temp;
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
 
-        mars = new int[N+1][M+1];
-        dp = new int[N+1][M+1];
-        temp = new int[2][M+2];
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
 
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j <= M; j++){
-                mars[i][j] = sc.nextInt();
+    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1,0};
+
+//    private static boolean isArea(int x, int y){
+//        return 0<=x && x<m && 0<=y && y<n;
+//    }
+
+
+
+
+    public static void main(String[] args) throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = stoi(st.nextToken());
+        int M = stoi(st.nextToken());
+        int[][] arr = new int[N][M];
+        int[][] dp = new int[N+1][M+1];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++){
+                arr[i][j] = stoi(st.nextToken());
             }
         }
 
-        dp[1][1] = mars[1][1];
+        dp[0][0] = arr[0][0];
+        for(int i=1; i<M; i++){
+            dp[0][i] = dp[0][i-1] + arr[0][i];
+        }
 
-        for(int j = 2; j <= M; j++) dp[1][j] = mars[1][j]+ dp[1][j-1];
+        int[][] temp = new int[2][M];
 
-        for(int i = 2; i <= N; i++) {
+        for(int i=1; i<N; i++){
 
-            temp[0][0] = dp[i-1][1];
-            for(int j = 1; j <= M; j++) {
-                temp[0][j] = Math.max(temp[0][j-1], dp[i-1][j])+ mars[i][j];
+            temp[0][0] = dp[i-1][0] + arr[i][0];
+            for(int j=1; j<M; j++){
+
+                temp[0][j] = Math.max(temp[0][j-1], dp[i-1][j]) + arr[i][j];
             }
 
-            temp[1][M+1] = dp[i-1][M];
-            for(int j = M; j >= 1; j--) {
-                temp[1][j] = Math.max(temp[1][j+1], dp[i-1][j]) + mars[i][j];
+            temp[1][M-1] = dp[i-1][M-1] + arr[i][M-1];
+
+            for(int j=M-2; j>=0; j--){
+                temp[1][j] = Math.max(temp[1][j+1], dp[i-1][j]) + arr[i][j];
             }
 
-            for(int j = 1; j <= M; j++) {
+            for(int j=0; j<M; j++){
                 dp[i][j] = Math.max(temp[0][j], temp[1][j]);
             }
         }
-        System.out.println(dp[N][M]);
 
-        sc.close();
+        System.out.println(dp[N-1][M-1]);
     }
+
+
 
 }
