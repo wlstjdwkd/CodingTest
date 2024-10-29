df8a1b5dbbe9412cb9e2b2f15f4a278da2c32fbb
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-29 22:20:06 +0900

    [Gold V] Title: 치킨 배달, Time: 176 ms, Memory: 15420 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,44 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.Point;
+import java.io.*;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int N,M;
-    static int[][] map;
-    static ArrayList<Point> person;
-    static ArrayList<Point> chicken;
-    static boolean[] open;
-    static int result = Integer.MAX_VALUE;
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 
-    static class Point{
-        int x;
-        int y;
-        Point(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
     }
 
-    public static void main(String[] args) throws IOException {
+    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1,0};
+
+//    private static boolean isArea(int x, int y){
+//        return 0<=x && x<m && 0<=y && y<n;
+//    }
+
+    private static int N,M;
+    private static int[][] map;
+
+    private static List<Point> person;
+    private static List<Point> chicken;
+    private static int ans;
+    private static boolean[] open;
+
+    public static void main(String[] args) throws IOException{
+
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
 
         map = new int[N][N];
         person = new ArrayList<>();
         chicken = new ArrayList<>();
 
-
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = stoi(st.nextToken());
+
                 if(map[i][j] == 1){
-                    person.add(new Point(i,j));
+                    person.add(new Point(i, j));
                 }
                 else if(map[i][j] == 2){
                     chicken.add(new Point(i, j));
@@ -46,38 +60,47 @@ public class Main {
             }
         }
 
+        ans = Integer.MAX_VALUE;
         open = new boolean[chicken.size()];
 
         dfs(0,0);
-        System.out.println(result);
+
+        System.out.println(ans);
+
     }
 
-    //치킨 백트래킹 써야할듯?
-    //dfs 할 때 마지막 return을 왜 이렇게 까먹지?
-    private static void dfs(int start, int cnt){
-        if(cnt == M){
-            int res=0;
+    private static void dfs(int start, int depth){
+        if(depth == M){
+            int res = 0;
             for(int i=0; i<person.size(); i++){
                 int temp = Integer.MAX_VALUE;
 
-                for(int j=0; j<chicken.size(); j++){
+                for(int j=0; j<chicken.size(); j++) {
                     if(open[j]){
-                        int distance = Math.abs(person.get(i).x - chicken.get(j).x)+ Math.abs(person.get(i).y-chicken.get(j).y);
+                        int distance =
+                                Math.abs(person.get(i).x - chicken.get(j).x) + Math.abs(person.get(i).y - chicken.get(j).y);
 
-                        temp = Math.min(temp,distance);
+                        temp = Math.min(temp, distance);
                     }
+
                 }
-                res +=temp;
+
+                res+=temp;
             }
 
-            result = Math.min(result,res);
+            ans = Math.min(ans, res);
             return;
         }
 
-        for(int i= start; i<chicken.size(); i++){
-            open[i]= true;
-            dfs(i+1, cnt+1);
+        for(int i = start; i<chicken.size(); i++){
+            open[i] = true;
+            dfs(i+1, depth+1);
             open[i] = false;
         }
+
+
     }
+
+
+
 }
