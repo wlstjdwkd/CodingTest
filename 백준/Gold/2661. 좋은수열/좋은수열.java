ce845d59e02f775582615b00222dbb2b213331b1
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-30 21:27:16 +0900

    [Gold IV] Title: 좋은수열, Time: 84 ms, Memory: 13056 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.java
+++ 백준/Gold/2661. 좋은수열/좋은수열.java
@@ -1,33 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.Point;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int N;
-    public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        backTracking("");
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
     }
-    public static void backTracking(String result){
-        if(result.length()==N){
-            System.out.println(result);
+
+    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1,0};
+
+//    private static boolean isArea(int x, int y){
+//        return 0<=x && x<m && 0<=y && y<n;
+//    }
+    private static final int start = 1;
+    private static final int end = 3;
+    private static int n;
+
+    public static void main(String[] args) throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+        n = stoi(st.nextToken());
+
+        backtracking("");
+
+    }
+
+    private static void backtracking(String s){
+        if(s.length() == n){
+            System.out.println(s);
             System.exit(0);
         }
-        for(int i = 1; i <= 3; i++){
-            if(isGood(result+i)){
-                backTracking(result+i);
-            }
 
+        for(int i = start; i <= end; i++){
+            if(canMake(s+i)){
+                backtracking(s+i);
+            }
         }
     }
-    public static boolean isGood(String s){
-        int length = s.length()/2;
-        for(int i=1; i<=length; i++){
-            if(s.substring(s.length()-i).equals(s.substring(s.length()-2*i, s.length()-i))){
+
+    private static boolean canMake(String s){
+        for(int i=1; i<=s.length()/2; i++){
+            String front = s.substring(s.length() - i*2, s.length() - i);
+            String end = s.substring(s.length() - i);
+            if(front.equals(end)){
                 return false;
             }
         }
+
         return true;
     }
+
 }
