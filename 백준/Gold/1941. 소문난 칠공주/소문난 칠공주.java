4732f8caf1728689cf0e7765e4ba88e1db4543b8
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-30 22:36:47 +0900

    [Gold III] Title: 소문난 칠공주, Time: 272 ms, Memory: 89104 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,71 +1,103 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.Point;
+import java.io.*;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    static char[][] map = new char[5][5];
-    static int[] dx ={-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static int[] combX = new int [25];
-    static int[] combY = new int [25];
-    static int ans = 0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for(int i =0; i<5; i++){
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1,0};
+
+    private static boolean isArea(int x, int y){
+        return 0<=x && x<5 && 0<=y && y<5;
+    }
+    private static char[][] map;
+    private static int answer;
+    private static boolean[] visited;
+    private static int[] selected = new int[7];
+
+    public static void main(String[] args) throws IOException{
+
+        map = new char[5][5];
+        for(int i=0; i<5; i++){
             map[i] = br.readLine().toCharArray();
         }
 
-        for(int i=0; i<25; i++){
-            combX[i] = i%5;
-            combY[i] = i /5;
-        }
+        backtracking(0,0,0);
 
-        combination(new int[7],0,0,7);
-        System.out.println(ans);
+        System.out.println(answer);
     }
 
-    private static void combination(int[] comb, int idx, int depth, int left) {
-        if(left ==0){
-            bfs(comb);
-            return;
-        }
-        if(depth == 25){
+    private static void backtracking(int depth, int numY, int start){
+        if(numY >=4){
             return;
         }
 
-        comb[idx] = depth;
-        combination(comb,idx+1, depth+1, left-1);//선택
-        combination(comb,idx,depth+1,left);//선택X
+        if(depth == 7){
+            visited = new boolean[7];
+            bfs();
+            return;
+        }
+
+        for(int i=start; i<25; i++){
+            selected[depth] = i;
+            if(map[i/5][i%5] == 'Y'){
+                backtracking(depth+1, numY+1, i+1);
+            }
+            else{
+                backtracking(depth+1, numY, i+1);
+            }
+        }
     }
 
-    private static void bfs(int[] comb) {
-        Queue<Integer>q = new ArrayDeque<>();
-        boolean visited[] = new boolean[7];
+    private static void bfs() {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[] {selected[0]/5, selected[0]%5});
         visited[0] = true;
-        q.add(comb[0]);
-        int cnt =1, sCnt=0;
+        int conn = 1;
 
         while(!q.isEmpty()){
-            int cur = q.poll();
-            if(map[combY[cur]][combX[cur]] == 'S') sCnt++;
-
+            int[] cur = q.poll();
+            int r = cur[0];
+            int c = cur[1];
             for(int i=0; i<4; i++){
-                for(int next =1; next<7; next++){
-                    if(!visited[next] && combX[cur]+dx[i] == combX[comb[next]] && combY[cur]+dy[i] == combY[comb[next]]){
-                        visited[next] = true;
-                        q.add(comb[next]);
-                        cnt++;
+                int nr = r+dx[i];
+                int nc = c+dy[i];
+                int ni = nr*5 + nc;
+
+                if(isArea(nr, nc)){
+                    for(int j=0; j<7; j++){
+                        if(!visited[j]){
+                            if(selected[j] == ni){
+                                q.add(new int[] {nr,nc});
+                                visited[j] = true;
+                                conn++;
+                            }
+                        }
                     }
                 }
             }
         }
 
-        if(cnt == 7){
-            if(sCnt>=4){
-                ans++;
-            }
+        if(conn == 7){
+            answer++;
         }
     }
+
 }
