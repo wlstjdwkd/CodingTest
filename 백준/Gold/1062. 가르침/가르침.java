8fdf8551b3d3ce033d2b1bffb05fa2b70abdd59c
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-30 09:41:04 +0900

    [Gold IV] Title: 가르침, Time: 252 ms, Memory: 13384 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,75 +1,106 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.awt.Point;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 
-    static int n, k;
-    static int max = Integer.MIN_VALUE;
-    static boolean[] visited;
-    static String[] word;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-        word = new String[n];
-        for(int i = 0; i < n; i++) {
-            String str = br.readLine();
-
-            str = str.replace("anta", "");
-            str = str.replace("tica", "");
-            word[i] = str;
-        }
-
-        if(k < 5) {
-            System.out.println("0");
-            return;
-        } else if(k == 26) {
-            System.out.println(n);
-            return;
-        }
-
-        visited = new boolean[26];
-        visited['a' - 'a'] = true;
-        visited['c' - 'a'] = true;
-        visited['i' - 'a'] = true;
-        visited['n' - 'a'] = true;
-        visited['t' - 'a'] = true;
-
-        dfs(0, 0);
-        System.out.println(max);
+    private static int stoi(String s){
+        return Integer.parseInt(s);
     }
 
-    public static void dfs(int alpha, int len) {
-        if(len == k - 5) {
-            int count = 0;
-            for(int i = 0; i < n; i++) {
-                boolean read = true;
-                for(int j = 0; j < word[i].length(); j++) {
-                    if(!visited[word[i].charAt(j) - 'a']) {
-                        read = false;
+    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1,0};
+
+//    private static boolean isArea(int x, int y){
+//        return 0<=x && x<m && 0<=y && y<n;
+//    }
+
+    private static int n, k;
+    private static String[] word;
+    private static boolean[] anta;
+    private static int max;
+
+    public static void main(String[] args) throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+
+        n = stoi(st.nextToken());
+        k = stoi(st.nextToken());
+        word = new String[n];
+        anta = new boolean[26];
+        anta['a' - 'a'] = true;
+        anta['n' - 'a'] = true;
+        anta['t' - 'a'] = true;
+        anta['i' - 'a'] = true;
+        anta['c' - 'a'] = true;
+
+        if(k<5){
+            System.out.println(0);
+        }
+        else if(k == 26){
+            System.out.println(n);
+        }
+        else{
+            for(int i=0; i<n; i++){
+                String str = br.readLine();
+                str = str.replace("anta", "");
+                str = str.replace("tica", "");
+
+                word[i] = str;
+            }
+
+            backtracking(0,0);
+
+            System.out.println(max);
+        }
+
+
+    }
+
+    private static void backtracking(int start, int depth){
+        if(depth == k-5){
+            int cnt = 0;
+            for(int i=0; i<n; i++){
+                String str = word[i];
+                boolean flag = true;
+                for(int j=0; j<str.length(); j++){
+                    char c = str.charAt(j);
+                    if(!anta[c-'a']){
+                        flag = false;
                         break;
                     }
                 }
-                if(read) count++;
+
+                if(flag){
+                    cnt++;
+                }
             }
-            max = Math.max(max, count);
+
+            max = Math.max(max, cnt);
             return;
         }
 
-        for(int i = alpha; i < 26; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                dfs(i, len + 1);
-                visited[i] = false;
+        for(int i=start; i<26; i++){
+            if(!anta[i]){
+                anta[i] = true;
+                backtracking(i+1, depth+1);
+                anta[i] = false;
             }
         }
     }
+
+
+
 }
