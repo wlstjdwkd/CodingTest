77fb7f5c1ede42975a43a5174482c4a35716660d
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-31 15:46:39 +0900

    [Gold IV] Title: 좋다, Time: 132 ms, Memory: 12772 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,57 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.Point;
+import java.io.*;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.Set;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] numbers = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            numbers[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(numbers);
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1,0};
+
+//    private static boolean isArea(int x, int y){
+//        return 0<=x && x<5 && 0<=y && y<5;
+//    }
+
+    private static int[] house;
+
+    public static void main(String[] args) throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = stoi(st.nextToken());
+        int[] arr = new int[n];
 
         int result = 0;
-        for(int i=0; i<N; i++){
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = stoi(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i=0; i<n; i++){
+            int findNum = arr[i];
             int left = 0;
-            int right = N-1;
-            while(true){
-                if(left==i){
-                    left++;
+            int right = n-1;
+            int sum = 0;
+            while(left < right){
+
+                sum = arr[left] + arr[right];
+                if(sum == findNum){
+                    if(i==left){
+                        left++;
+                    }
+                    else if(right == i){
+                        right--;
+                    }
+                    else{
+                        result++;
+                        break;
+                    }
+
                 }
-                else if(right == i){
+                sum = arr[left] + arr[right];
+
+                if(sum > findNum){
                     right--;
                 }
-
-                if(left>=right){
-                    break;
-                }
-
-                if(numbers[left] + numbers[right] > numbers[i]){
-                    right--;
-                }
-                else if(numbers[left] + numbers[right] < numbers[i]){
+                else if(sum < findNum){
                     left++;
-                }
-                else{
-                    result++;
-                    break;
                 }
             }
+
+
         }
         System.out.println(result);
     }
+
 }
