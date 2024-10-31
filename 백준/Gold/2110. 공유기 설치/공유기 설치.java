161cb74e08bdc33389095b45344d515a6d1c5b30
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-10-31 15:18:56 +0900

    [Gold IV] Title: 공유기 설치, Time: 240 ms, Memory: 28500 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,51 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.Point;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int N,C;
-    static int arr[];
-    public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        arr= new int[N];
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1,0};
+
+//    private static boolean isArea(int x, int y){
+//        return 0<=x && x<5 && 0<=y && y<5;
+//    }
+
+    private static int[] house;
+
+    public static void main(String[] args) throws IOException{
+
+        st = new StringTokenizer(br.readLine());
+        int N = stoi(st.nextToken());
+        int M = stoi(st.nextToken());
+
+        house = new int[N];
         for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+            house[i] = stoi(br.readLine());
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(house);
 
-        int max=0;
+        int lo = 1;
+        int hi = house[N-1] - house[0] + 1;
 
-        int left=1;
-        int right = arr[N-1]-arr[0];
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
 
-        while(left<=right){
-            int mid = (left+right)/2;
-            int start = arr[0];
-            int cnt=1;
-
-            for(int i=1; i<N; i++){
-                if(arr[i]-start>=mid){
-                    cnt++;
-                    start = arr[i];
-                }
-            }
-
-            if(cnt>=C){
-                max=mid;
-                left = mid+1;
+            if(canInstall(mid)<M){
+                hi = mid - 1;
             }
             else{
-                right = mid-1;
+                lo = mid + 1;
             }
         }
-        System.out.println(max);
+
+        System.out.println(lo-1);
     }
+
+    private static int canInstall(int distance){
+        int count = 1;
+        int lastLocate = house[0];
+
+        for(int i=1; i<house.length; i++){
+            int locate = house[i];
+            if(locate - lastLocate >= distance){
+                count++;
+                lastLocate = locate;
+            }
+        }
+
+        return count;
+    }
+
 }
