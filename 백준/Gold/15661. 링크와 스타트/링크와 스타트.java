c018e815bd3bb0386645dfc959555c3e9fe7ed3c
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-11-01 13:15:11 +0900

    [Gold V] Title: 링크와 스타트, Time: 652 ms, Memory: 16804 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -1,72 +1,96 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.Point;
+import java.io.*;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.Set;
 import java.util.Stack;
 import java.util.StringTokenizer;
+import org.w3c.dom.Node;
 
 public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
 
 
-    static int N, result;
-    static int[][] player;
-    static boolean[] visit;
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        player = new int[N][N];
-        visit = new boolean[N];
-        result = Integer.MAX_VALUE;
-        for (int i = 0; i < N; i++) {
+    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1,0};
+
+//    private static boolean isArea(int x, int y){
+//        return 0<=x && x<n && 0<=y && y<n;
+//    }
+
+    private static int[][] map;
+    private static int n, answer, t;
+    private static boolean[] v;
+
+    public static void main(String[] args) throws IOException{
+
+        n = stoi(br.readLine());
+        map = new int[n][n];
+        v = new boolean[n];
+        answer = Integer.MAX_VALUE;
+        t = 0;
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                player[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < n; j++){
+                map[i][j] = stoi(st.nextToken());
             }
         }
 
-        solve(0);
-        System.out.println(result);
-    }
-    private static void solve(int cnt){
-        if(cnt == N){
-            int start = 0;
-            int link = 0;
-            for(int i=0; i<N; i++){
-                for(int j=i+1; j<N; j++){
-                    if(visit[i] && visit[j]){
-                        start += player[i][j];
-                        start += player[j][i];
-                    }
-                    else if(!visit[i] && !visit[j]){
-                        link += player[i][j];
-                        link += player[j][i];
-                    }
-                }
-            }
+        for(t=1; t<=n/2; t++){
+            nCr(0,0);
+        }
 
-            int diff = Math.abs(start - link);
-            result = Math.min(result, diff);
+        System.out.println(answer);
+    }
+
+    private static void nCr(int depth, int start){
+        if(depth == t){
+            answer = Math.min(answer,diff());
+
+            if(answer == 0){
+                System.out.println(0);
+                System.exit(0);
+            }
 
             return;
         }
 
-//        for(int i=idx; i<N; i++){
-//            if(!visit[i]){
-//                visit[i] = true;
-//                solve(i+1, cnt+1);
-//                visit[i] = false;
-//            }
-//        }
-
-        visit[cnt] = true;
-        solve(cnt+1);
-        visit[cnt] = false;
-        solve(cnt+1);
+        for(int i=start; i<n; i++){
+            if(!v[i]){
+                v[i] = true;
+                nCr(depth+1, i+1);
+                v[i] = false;
+            }
+        }
     }
+
+    private static int diff(){
+        int start = 0;
+        int link = 0;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(v[i] && v[j]){
+                    start += (map[i][j] + map[j][i]);
+                }
+                else if(!v[i] && !v[j]){
+                    link += (map[i][j] + map[j][i]);
+                }
+            }
+        }
+
+        return Math.abs(start - link);
+    }
+
+
 }
