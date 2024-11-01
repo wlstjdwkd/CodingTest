3f15ede986417d0087dcdca3157b3ab6b865cad4
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-11-01 23:39:35 +0900

    [Gold V] Title: 개똥벌레, Time: 420 ms, Memory: 33584 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -1,51 +1,86 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.Point;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
 import java.util.StringTokenizer;
+import org.w3c.dom.Node;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int N,H, up[], down[];
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+
+
+    private static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    private static final int[] dx = {0,1,0,-1};
+    private static final int[] dy = {-1,0,1,0};
+
+//    private static boolean isArea(int x, int y){
+//        return 0<=x && x<n && 0<=y && y<n;
+//    }
+
+
+
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-        up = new int[N/2];
-        down = new int[N/2];
-        for(int i=0; i<N/2; i++){
-            down[i] = Integer.parseInt(br.readLine());
-            up[i] = Integer.parseInt(br.readLine());
+        st=new StringTokenizer(br.readLine());
+
+        int n = stoi(st.nextToken());
+        int h = stoi(st.nextToken());
+
+        int[] down = new int[n/2];
+        int[] up = new int[n/2];
+        for(int i=0; i<n/2; i++){
+            int a = stoi(br.readLine());
+            int b = stoi(br.readLine());
+
+            down[i] = a;
+            up[i] = b;
         }
-        Arrays.sort(down);
+
         Arrays.sort(up);
+        Arrays.sort(down);
 
-        int min = N;
-        int cnt=0;
-        for(int i=1; i<H+1; i++) {
-            int conflict =binarySearch(0, N/2, i, down) + binarySearch(0, N/2, H-i+1, up);
-            if(min == conflict) {
+        int min = n;
+        int cnt = 0;
+
+        for(int i=1; i<h+1; i++){
+            int conflict = binarySearch(0, n/2, i, down) + binarySearch(0, n/2, h-i+1, up);
+            if(min == conflict){
                 cnt++;
-                continue;
             }
-            if(min > conflict) {
+            else if(min> conflict){
                 min = conflict;
-                cnt=1;
+                cnt = 1;
             }
         }
-        System.out.println(min +" " +cnt);
-    }
-    static int binarySearch(int left, int right, int h, int[] arr) {
-        while(left<right) {
-            int mid = (left+right)/2;
 
-            if(arr[mid] >= h) {
+        System.out.println(min + " " + cnt);
+
+    }
+
+    private static int binarySearch(int left, int right, int h, int[] arr){
+        while(left < right){
+            int mid = (left + right) / 2;
+            if(arr[mid] >= h){
                 right = mid;
-            }else {
-                left = mid+1;
+            }
+            else{
+                left = mid + 1;
             }
         }
-        return arr.length-right;
+
+        return arr.length - right;
     }
+
+
 }
